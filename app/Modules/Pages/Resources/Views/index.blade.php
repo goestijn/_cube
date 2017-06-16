cc136d711e19135d10f033e31698d9da0d034880
goestijn <stijn@goethals-degraeve.net>
2017-06-16 06:13:37 +0200

    Continue working on admin-design + setup page module

---
+++ app/Modules/Pages/Resources/Views/index.blade.php
@@ -0,0 +1,20 @@
+@extends('admin::layouts.master')
+
+
+@section('content')
+	
+	<header>
+
+		<div class="container container-alt">
+			
+			<h2>Pages</h2>
+			<nav class="breadcrumb">
+	            <a class="breadcrumb-item" href="/admin">Home</a>
+				<span class="breadcrumb-item active">File-manager</span>
+	        </nav>
+
+		</div>
+		
+	</header>
+
+@endsection
