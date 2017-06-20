3bf8805fb8dd32333143d810f43475f39b9b3126
Stijn Goethals <stijn@Stijns-MacBook-Pro-2.local>
2017-06-20 17:27:55 +0200

    Optimize user logout

--- app/Modules/Admin/Resources/Views/layouts/default.blade.php
+++ app/Modules/Admin/Resources/Views/layouts/default.blade.php
@@ -7,7 +7,7 @@
 
             <header class="header">
                 
-                <div class="navigation-trigger hidden-xl-up" data-ma-action="aside-open" data-ma-target=".sidebar">
+                <div class="navigation-trigger hidden-xl-up" data-action="aside-open" data-target=".sidebar">
                     <div class="navigation-trigger__inner">
                         <i class="navigation-trigger__line"></i>
                         <i class="navigation-trigger__line"></i>
@@ -27,7 +27,7 @@
 
                     <div class="user">
 
-                        <div class="user__info" data-toggle="dropdown">
+                        <div class="user__info" data-action="navigation-user-sub-toggle">
                             <img class="user__img" src="" alt="">
                             <div>
                                 <div class="user__name">{{ Auth::user()->fullname }}</div>
@@ -35,10 +35,11 @@
                             </div>
                         </div>
 
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item" href="">View Profile</a>
-                            <a class="dropdown-item" href="">Settings</a>
-                            <a class="dropdown-item" href="/admin/logout">Logout</a>
+                        <div class="navigation__sub">
+                            <ul>
+                                <li><a href=""><i class="fa fa-vcard-o"></i>View Profile</a></li>
+                                <li><a href="/admin/logout" data-action="logout"><i class="fa fa-sign-out"></i>Logout</a></li>
+                            </ul>
                         </div>
 
                     </div>
@@ -48,7 +49,7 @@
                         <li><a href="index.html"><i class="fa fa-dashboard"></i> Dashboard</a></li>
 
                         <li class="navigation__sub">
-                            <a href=""><i class="fa fa-pencil-square-o"></i> Content</a>
+                            <a href="" data-action="navigation-sub-toggle"><i class="fa fa-pencil-square-o"></i> Content</a>
                             <ul>
                                 <li><a href="hidden-sidebar.html">Pages</a></li>
                                 <li><a href="boxed-layout.html">Blog</a></li>
@@ -95,7 +96,79 @@
     
     <script>
 
+        body.on('click', '[data-action]', function (e) {
 
+            e.preventDefault();
+
+            var $this = $(this);
+            var action = $this.data('action');
+            var target = '';
+
+            switch (action) {
+
+                /*-------------------------------------------
+                    Aside
+                ---------------------------------------------*/
+                case 'aside-open':
+
+                    target = $this.data('target');
+                    $this.addClass('toggled');
+                    $(target).addClass('toggled');
+                    $('.content, .header').append('<div class="ma-backdrop" data-action="aside-close" data-target='+target+' />');
+
+                    break;
+
+
+                case 'aside-close':
+
+                    target = $this.data('target');
+                    $('[data-action="aside-open"], '+target).removeClass('toggled');
+                    $('.content, .header').find('.ma-backdrop').remove();
+
+                    break;
+
+                /*-------------------------------------------
+                    Logout 
+                ---------------------------------------------*/
+                case 'logout':
+
+                    swal({
+                        title: 'Do you really want to leave?',
+                        text: 'Are you sure you want to sign out from the application?',
+                        type: 'question',
+                        showCancelButton: true,
+                        buttonsStyling: false,
+                        confirmButtonClass: 'btn btn-primary',
+                        confirmButtonText: 'Yes, Sign me out',
+                        cancelButtonClass: 'btn btn-secondary',
+                        cancelButtonText: 'No',
+                    }).then(function(){
+
+                        window.location.href = $(e.target).attr('href');
+                    
+                    });
+
+                    break;
+
+                /*------------------------------------------------
+                    Sidebar toggle menu
+                -------------------------------------------------*/
+                case 'navigation-sub-toggle':
+
+                    $(this).parent().toggleClass('navigation__sub--toggled');
+                    $(this).next('ul').slideToggle(250);
+
+                    break;
+
+                case 'navigation-user-sub-toggle':
+
+                    $(this).next().toggleClass('navigation__sub--toggled');
+                    $(this).next().children('ul').slideToggle(250);
+                    break;
+
+            }
+
+        });
         
     </script>
 
--- app/Modules/Admin/Resources/Views/layouts/master.blade.php
+++ app/Modules/Admin/Resources/Views/layouts/master.blade.php
@@ -5,12 +5,14 @@
 
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <!-- vendors -->
         <link rel="stylesheet" href="{{ asset('vendors/admin/bower_components/material-design-iconic-font/dist/css/material-design-iconic-font.min.css') }}">
         <link rel="stylesheet" href="{{ asset('vendors/admin/bower_components/font-awesome/css/font-awesome.min.css') }}">
         <link rel="stylesheet" href="{{ asset('vendors/admin/bower_components/animate.css/animate.min.css') }}">
         <link rel="stylesheet" href="{{ asset('vendors/admin/bower_components/jquery.scrollbar/jquery.scrollbar.css') }}">
+        <link rel="stylesheet" href="{{ asset('vendors/admin/bower_components/sweetalert2/dist/sweetalert2.min.css') }}">
 
         <!-- fonts -->
         <link rel="stylesheet" href="{{ asset('fonts/admin/roboto/font-face.css') }}">
@@ -31,10 +33,16 @@
         <script src="{{ asset('vendors/admin/bower_components/Waves/dist/waves.min.js') }}"></script>
         <script src="{{ asset('vendors/admin/bower_components/jquery.scrollbar/jquery.scrollbar.min.js') }}"></script>
         <script src="{{ asset('vendors/admin/bower_components/jquery-scrollLock/jquery-scrollLock.min.js') }}"></script>
+        <script src="{{ asset('vendors/admin/bower_components/sweetalert2/dist/sweetalert2.min.js') }}"></script>
 
         <!-- js -->
-        <script src="{{ asset('js/admin/actions.js') }}"></script>
-        <script src="{{ asset('js/admin/app.js') }}"></script>
+        <!--<script src="{{ asset('js/admin/actions.js') }}"></script>
+        <script src="{{ asset('js/admin/app.js') }}"></script>-->
+        <script>
+            
+            var body = $('body');            
+
+        </script>
 
         @yield('script')
 
